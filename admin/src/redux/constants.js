ef41d7351b724cca8ace5a29dde89f4d06aa7ddf
Pkpedram <pkpedram80@gmail.com>
2024-01-24 01:43:22 +0330

    change url admoin

--- admin/src/redux/constants.js
+++ admin/src/redux/constants.js
@@ -1,9 +1,14 @@
-
-export const serverUrl = '';
+export const serverUrl = "";
 export const ApiConfig = {
-    apiKey: '',
-    authDomain: '',
-    domain: process.env.NODE_ENV == 'development' ?  'http://localhost:5000/' : 'https://configuratorapi.noav.dev/',
-    baseUrl: process.env.NODE_ENV == 'development' ? 'http://localhost:5000/api' : 'https://configuratorapi.noav.dev/api',
-    authUrl: '',
-  };
+  apiKey: "",
+  authDomain: "",
+  domain:
+    process.env.NODE_ENV == "development"
+      ? "http://arianaback.noavdev.ir/"
+      : "https://configuratorapi.noav.dev/",
+  baseUrl:
+    process.env.NODE_ENV == "development"
+      ? "http://arianaback.noavdev.ir/api"
+      : "https://configuratorapi.noav.dev/api",
+  authUrl: "",
+};
